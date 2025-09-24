38101594f535335185f43a5b6fbb7aea03bac3be
Aman Kumar <iamankumar279@gmail.com>
2025-09-24 23:23:18 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -11,6 +11,13 @@ app.use(express.urlencoded({limit:"16kb", extended:true}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes import
 
+import userRouter from "./routes/user.routes.js";
+
+// routes delcaration
+app.use("/api/v1/users",userRouter)
+
+//https:localhost:8000/api/v1/users/resister
 
 export{app}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import connectDB from "./db/db.js";
 import mongoose from "mongoose";
+import { app } from "./app.js";
 import dotenv from "dotenv";
 dotenv.config({
     path: "./.env"
